4e1866211dd7ae7d90113f4595c227ca2f93fea0
ottogudens <ottogudenschwager@gmail.com>
2026-06-28 01:36:54 -0400

    feat: initialize project settings with django-environ support and default configurations

--- backend/automaster/settings.py
+++ backend/automaster/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOW_ALL_ORIGINS = env.bool('CORS_ALLOW_ALL_ORIGINS', default=False)
+CORS_ALLOW_ALL_ORIGINS = env.bool('CORS_ALLOW_ALL_ORIGINS', default=True)
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     "http://localhost:5173",
     "http://localhost:3000",
+    "https://mecania.skale.cl",
 ])
 
 REST_FRAMEWORK = {
